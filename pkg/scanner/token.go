cf0b2d9c702e00795ec21d0355b3cca320dbb09f
rskaindoy <rskaindoy@up.edu.ph>
2026-09-12 02:38:05 +0800

    feat: define initial token types

---
+++ pkg/scanner/token.go
@@ -0,0 +1,63 @@
+// defines the types and structure of tokens produced by the scanner
+
+package scanner
+
+type TokenType int
+
+const (
+	// Single-character tokens
+	LEFT_PAREN TokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	SEMICOLON
+
+	// Arithmetic operators
+	PLUS
+	MINUS
+	STAR
+	SLASH
+	PERCENT
+
+	// Logical operators
+	AND
+	OR
+	NOT
+
+	// Assignment & Comparison operators
+	EQUAL
+	LESS
+	GREATER
+	EQUAL_EQUAL
+	NOT_EQUAL
+	LESS_EQUAL
+	GREATER_EQUAL
+
+	// Identifier & Literals
+	IDENTIFIER
+	STRING
+	NUMBER
+	TRUE
+	FALSE
+	NIL
+
+	// Flow control keywords
+	IF
+	ELSE
+
+	// TODO: More Witchcraft-themed keywords
+	BREW
+	SPELL
+	CAST
+
+	EOF
+)
+
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal any
+	Line    int
+}
